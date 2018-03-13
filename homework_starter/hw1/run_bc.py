2bce02a8c0b2cdf4471e5f8088b0b43c8d2a592e
John <johnwilles@gmail.com>
2018-03-12 23:49:50 -0400

    Rework bc class

--- homework_starter/hw1/run_bc.py
+++ homework_starter/hw1/run_bc.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-"""
-Code to load an expert policy and generate roll-out data for behavioral cloning.
-Example usage:
-    python run_expert.py experts/Humanoid-v1.pkl Humanoid-v1 --render \
-            --num_rollouts 20
-
-Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
-"""
-
 import pickle
 import tensorflow as tf
 import numpy as np
